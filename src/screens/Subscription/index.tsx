823834738941f15dc0f9c100aa5e979063f65c04
Mohammed Boudad <mboudad7@gmail.com>
2022-11-24 15:50:11 +0100

    Add monthly subscriptions

--- src/screens/Subscription/index.tsx
+++ src/screens/Subscription/index.tsx
@@ -37,31 +37,81 @@ const Subscription = () => {
             </Button>
           </HStack>
           <VStack mt={16} space={4} w="full">
-            <Card type="Free" advantage="Max 150 prodotti" />
-            <Card
-              type="Vetrina"
-              advantage="Max 150 prodotti"
-              price={109}
-              bonus="2 mesi in regalo"
-            />
-            <Card
-              type="Negozio"
-              advantage="Max 150 prodotti"
-              price={209}
-              bonus="2 mesi in regalo"
-            />
-            <Card
-              type="Vetrina"
-              advantage="Max 150 prodotti"
-              price={399}
-              bonus="2 mesi in regalo"
-            />
+            {switchOn
+              ? yearlySubscriptions.map((subscription, index) => (
+                  <Card
+                    key={index}
+                    type={subscription.type}
+                    price={subscription.price}
+                    advantage={subscription.advantage}
+                    bonus={subscription.bonus}
+                  />
+                ))
+              : monthlySubscriptions.map((subscription, index) => (
+                  <Card
+                    key={index}
+                    type={subscription.type}
+                    price={subscription.price}
+                    advantage={subscription.advantage}
+                    bonus={subscription.bonus}
+                  />
+                ))}
           </VStack>
         </VStack>
       </View>
     </Layout>
   );
 };
+
+const yearlySubscriptions = [
+  {
+    type: 'Free',
+    advantage: 'Max 150 prodotti',
+  },
+  {
+    type: 'Vetrina',
+    advantage: 'Max 150 prodotti',
+    price: 109,
+    bonus: '2 mesi in regalo',
+  },
+  {
+    type: 'Negozio',
+    advantage: 'Max 150 prodotti',
+    price: 209,
+    bonus: '2 mesi in regalo',
+  },
+  {
+    type: 'Vetrina',
+    advantage: 'Max 150 prodotti',
+    price: 399,
+    bonus: '2 mesi in regalo',
+  },
+];
+
+const monthlySubscriptions = [
+  {
+    type: 'Free',
+    advantage: 'Max 10 prodotti',
+  },
+  {
+    type: 'Vetrina',
+    advantage: 'Max 10 prodotti',
+    price: 19,
+    bonus: '2 mesi in regalo',
+  },
+  {
+    type: 'Negozio',
+    advantage: 'Max 10 prodotti',
+    price: 29,
+    bonus: '2 mesi in regalo',
+  },
+  {
+    type: 'Vetrina',
+    advantage: 'Max 10 prodotti',
+    price: 39,
+    bonus: '2 mesi in regalo',
+  },
+];
 
 const styles = StyleSheet.create({
   title: {
